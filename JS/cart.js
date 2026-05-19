1edb3f83b846179ba58b7c20116f0d93346f6d5e
VERONESS <turokromaha@mail.ru>
2026-05-19 07:39:27 +0300

    Add files via upload

---
+++ JS/cart.js
@@ -0,0 +1,16 @@
+const cart = function(){
+    const cartBtn = document.querySelector('.button-cart')
+    const cart = document.querySelector('#modal-cart')
+    const closeBtn = document.querySelector('.modal-close')
+    console.log(cart)
+        
+    cartBtn.addEventListener('click', function (){
+        cart.style.display = 'flex'
+    })
+    closeBtn.addEventListener('click', function (){
+        cart.style.display = ''
+    })
+}
+cart()
+
+
---
+++ JS/search.js
@@ -0,0 +1,28 @@
+const search = function(){
+    const input = document.querySelector('.search-block > input')
+    console.log(input)
+    const searchBtn = document.querySelector('search.block > button')   
+    input.addEventListener('input', (event) => {
+         console.log(event.target.value)  
+    })
+}
+search()
+
+const wmn = function(){
+    const a = document.querySelector('.navigation-item')
+ console.log(a)
+ a.addEventListener('click', function (){
+    console.log(a.value)
+ })
+}
+wmn()
+
+const searchBtn = function(){
+    const b = document.getElementById('button-addon2')
+ console.log(b)
+b.addEventListener('click', function(){
+    console.log(b.value)
+    console.log(b)
+})
+}
+searchBtn()
